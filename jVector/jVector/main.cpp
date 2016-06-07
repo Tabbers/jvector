f139c0f44887ed774e5b694524433ce85313b59c
Tabbers <josua_kucher@yahoo.de>
2016-06-07 02:43:46 +0200

    beginnings

--- jVector/jVector/main.cpp
+++ jVector/jVector/main.cpp
@@ -1,52 +1,144 @@
-
 #include <cstdint>
+#include <memory>
+typedef unsigned int uint;
+
+
+namespace
+{
+	template<class T>
+	struct CombinedPointer
+	{
+		char* m_memory;
+		T* m_data;
+	};
+
+	template<class T>
+	static CombinedPointer<T> allocateMemory(uint numberOfObjects)
+	{
+		size_t memorytoAllocate = sizeof(T)*numberOfObjects;
+		size_t alignement = alignof(T);
+
+		char* unalignedMemory = reinterpret_cast<char*>(malloc(memorytoAllocate + alignement));
+		size_t difference = reinterpret_cast<size_t>(pOrigin) % alignment;
+
+		if (difference != 0)
+			memory = memorytoAllocate + (alignment - difference);
+		else
+			memory = memorytoAllocate;
+
+		CombinedPointer<T> pointers;
+		pointers.m_memory = unalignedMemory;
+		pointers.m_data = reinterpret_cast<T*> memory;;
+
+		return pointers;
+	}
+}
 
 template<class T>
 class JVector
 {
+	
 	public:
-		void reserve(size_t);
-		void push_back(T);
-		void resize(size_t);
-		void erease(unsigned int);
-		void erease_by_swap(unsigned int);
-	private:
-		size_t capacity;
-		size_t size;
 
-		uintptr_t memory;
+		uint capacity() const;
+		uint size() const;
+
+		void reserve(uint);
+		void push_back(T);
+		void resize(uint);
+		void erease(uint);
+		void erease_by_swap(uint);
+	private:
+		size_t m_capacity;
+		size_t m_size;
+
+		char* m_memory;
+		T* m_data;
+
+	private:
+	
+
 };
 
+
 template<class T>
-void JVector<T>::reserve(size_t numberOfObjects)
+uint JVector<T>::capacity() const
 {
+	return m_capacity;
+}
+
+template<class T>
+uint JVector<T>::size() const
+{
+	return m_size();
+}
+
+template<class T>
+void JVector<T>::reserve(uint numberOfObjects)
+{
+	if (numberOfObjects <= capacity)
+		return;
+
+	CombinedPointer<T> newMemory = allocateMemory(numberOfObjects);
+
+	if (m_size > 0)
+	{
+		for (uint i = 0u; i < m_size; ++i)
+		{
+			new (&newMemory.m_data[i]) T(m_data[i]);
+		}
+		m_memory = newMemory.m_memory;
+		m_data = newMemory.m_data;
+	}
+	else
+	{
+		m_memory = newMemory.m_memory;
+		m_data = newMemory.m_data;
+	}
 }
 
 template<class T>
 void JVector<T>::push_back(T)
 {
-	size_t newSize = size + sizeof(T);
+	size_t newSize = m_size + sizeof(T);
 	if (newSize >= capacity)
 	{
-		//need to allocate new memory here
+		CombinedPointer<T> newMemory = allocateMemory(newSize*2);
+		for (uint i = 0u; i < m_size; ++i)
+		{
+			new (&newMemory.m_data[i]) T(m_data[i]);
+		}
+		m_memory = newMemory.m_memory;
+		m_data = newMemory.m_data;
 	}
 }
 
 template<class T>
-void JVector<T>::resize(size_t numberOfObjects)
+void JVector<T>::resize(uint numberOfObjects)
 {
-	// allocate new Memory
-	// construct T in
+	if (m_size > 0)
+	{
+		
+	}
+	else
+	{
+
+	}
 
 }
 
 template<class T>
-void JVector<T>::erease(unsigned int indexToDelete)
+void JVector<T>::erease(uint indexToDelete)
 {
 	
 }
 
 template<class T>
-void JVector<T>::erease_by_swap(unsigned int indexToDelete)
+void JVector<T>::erease_by_swap(uint indexToDelete)
 {
+}
+
+int main()
+{
+	return 0;
 }
